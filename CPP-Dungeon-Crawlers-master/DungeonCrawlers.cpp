96e524b3857fed728e225a23607c1c8ed929aa54
Alex <31144589+whacky1542@users.noreply.github.com>
2018-05-03 14:22:15 -0600

    THIS ONE
    Up to date code

--- CPP-Dungeon-Crawlers-master/DungeonCrawlers.cpp
+++ CPP-Dungeon-Crawlers-master/DungeonCrawlers.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int gameStart();
-void dialogueBox();
+int dialogueBox();
 
 int main()
 {
+	string location;
 	int mainMenuInput = 0;
 	bool invalid = true;
 	while (invalid == true)
@@ -16,6 +17,8 @@ int main()
 		if (mainMenuInput == 1)
 		{
 			cout << "game started";
+			dialogueBox();
+			location = "intro1"
 			invalid = false;
 		}
 		else if (mainMenuInput != 2)
@@ -43,8 +46,20 @@ int gameStart()
 	return input;
 }
 
-void dialogueBox()
+int dialogueBox()
 {
+	open file ""insert location".txt"
+	dialogueCode = location;
 	cout << "Insert Scenario Here";
+}
+
+void intro1()
+{
+	cout << "Ralf -" << endl << "Hello adventurer! Welcome to the town Narnia! My name is Ralf and I’ll be your guide to DUNGEON CRAWLERS. Your goal is to gain wealth and fame by delving the endless amount of dungeons just on the edge of town. It won’t be easy to do it by your lonesome, so go over to the Blacksmith for weapons and armor or the Alchemist for potions. When you are ready to delve your first dungeon, just give the word." << endl;
+	cout << "1 - Give the word" << endl;
+	cout << "2 - Go to the Blacksmith" << endl;
+	cout << "3 - Go to the Alchemist" << endl;
+	cout << "4 - Check Inventory" << endl;
+	cout << "5 - Quit" << endl << ">";
 }
 
